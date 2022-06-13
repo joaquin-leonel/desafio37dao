e5c1e336d7cc490410472a23368618fc9d312554
joaquin leonel <woriltomo10@gmail.com>
2022-06-13 13:13:57 -0300

    desafio 37 dao

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const passport = require("passport");
+const sessionsRouter = express.Router()
+const {logIn, logOut, signUp, failureSignUp, failureLogIn} = require('../controller/auth')
+const upload = require('../utils/fileUpload')
+
+sessionsRouter
+            .post('/login', passport.authenticate('login', {failureRedirect: '/wrong-login', successRedirect: '/productos'}))
+            .post('/signup',upload.single('avatarFile'), passport.authenticate('signup', {failureRedirect: '/user-exist', successRedirect: '/productos'}))
+            .get('/login', logIn)
+            .get('/logout', logOut)
+            .get('/signup', signUp)
+            .get('/user-exist', failureSignUp)
+            .get('/wrong-login', failureLogIn)
+
+            
+module.exports = sessionsRouter;
